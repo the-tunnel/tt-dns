eaf7d07eda592a1384128dd7bc3938e2267068a5
elderking <test@example.com>
2019-12-09 15:57:08 +0800

    use random cookie

--- src/core.rs
+++ src/core.rs
@@ -1,6 +1,4 @@
-#![allow(dead_code)]
-#![allow(non_camel_case_types)]
-
+extern crate rand;
 use std::thread;
 use std::process;
 use std::sync::{Arc, Mutex};
@@ -10,12 +8,6 @@ use std::net::{SocketAddr, UdpSocket};
 
 struct TT_DNS_STATE {
     client_addr : SocketAddr,
-//    forwarded   : bool,
-//    finished    : bool,
-//    request_buf : Vec<u8>,
-//    request_len : u16,
-//    response_buf: Vec<u8>,
-//    response_len: u16,
 }
 
 pub fn run(LISTEN: &str, UPSTREAM: &str) -> std::io::Result<()> {
@@ -27,18 +19,19 @@ pub fn run(LISTEN: &str, UPSTREAM: &str) -> std::io::Result<()> {
             process::exit(-1);
         });
         let socket_upstream = UdpSocket::bind("0.0.0.0:0")?;
-        socket_upstream.connect(UPSTREAM)?;
+        socket_upstream.connect(UPSTREAM).unwrap_or_else(|err|{
+            eprintln!("Error connecting to: [{}], {}", UPSTREAM, err);
+            process::exit(-1);
+        });
 
         let map: HashMap<u16, TT_DNS_STATE> = HashMap::with_capacity(65536);
         let map = Arc::new(Mutex::new(map));
-
 
         let _map = map.clone();
         let _socket_listen = socket_listen.try_clone().unwrap();
         let _socket_upstream = socket_upstream.try_clone().unwrap();
         let upstream = thread::spawn(move || {
             let mut buf = [0u8; 2048];
-
             loop {
                 let (len, addr) = _socket_listen.recv_from(&mut buf).unwrap();
                 let trans_id = ((buf[0] as u16) << 8) + buf[1] as u16;
@@ -49,7 +42,6 @@ pub fn run(LISTEN: &str, UPSTREAM: &str) -> std::io::Result<()> {
                 //else {
                     _map.lock().unwrap().insert(trans_id, tt_dns);
                 //}
-
                 let new_len = append_OPT_record(&mut buf, len).unwrap();
                 _socket_upstream.send(&buf[..new_len]).unwrap();
             }
@@ -80,12 +72,10 @@ pub fn run(LISTEN: &str, UPSTREAM: &str) -> std::io::Result<()> {
 pub fn check_OPT_record(packet: &[u8], _len: usize) -> bool {
     let additional_RRs = ((packet[10] as u16) << 8) + packet[11] as u16;
     if additional_RRs == 0 {
-        false
+        return false
     }
-    else {
-        // we shall strictly check the OPT record, but it works for now.
-        true
-    }
+    // we shall strictly check the OPT record, but it works for now.
+    true
 }
 
 pub fn append_OPT_record(packet: &mut [u8], len: usize) -> Result<usize, Box<dyn Error>> {
@@ -94,11 +84,13 @@ pub fn append_OPT_record(packet: &mut [u8], len: usize) -> Result<usize, Box<dyn
     }
 
     packet[11] += 1;            // additional_RRs ++
-
-    // additional records header
-    packet[len..len+11].copy_from_slice(&[0x00,0x00,0x29,0x10,0x00,0x00,0x00,0x00,0x00, 0x00,0x0c]);
     // 12 (0x0c) bytes of record = OPTION Code: 0x000a (cookie) + OPTION len: 0x0008
-    // 8 bytes of COOKIE should be random, does not matter for now.
-    packet[len+11..len+11+12].copy_from_slice(&[0x00,0x0a,0x00,0x08, 1,2,3,4,5,6,7,8]);
-    Ok(len+11+12)
+    // 8 bytes of COOKIE should be random
+    packet[len..len+15].copy_from_slice(&[0x00,0x00,0x29,0x10,0x00,0x00,0x00,0x00,0x00,0x00,0x0c, 0x00,0x0a,0x00,0x08]);
+    let mut count = 0;
+    while count < 8 {
+        packet[len+11+4+count] = rand::random::<u8>();
+        count += 1;
+    }
+    Ok(len+11+4+8)
 }
